8e4732951f9c3d6d101693a0ba6b2ce87e6dfea3
bhavanianant <bhavani.ananthabhotla@yale.edu>
2017-06-13 17:06:13 -0400

    Add files via upload

--- scrapeLyrics.py
+++ scrapeLyrics.py
@@ -39,33 +39,18 @@ for composer in composerSites.iterkeys():
 	compositions = map(lambda c: "http://www.karnatik.com/" + c['href'], compositions)
 
 	for composition in compositions:
-		# composition = compositions[0]
-		# print composition
 		songPage = requests.get(composition, proxies=proxies)
-		# print songPage
 		if(songPage.status_code != 200):
 			print "Error: could not get webpage: " + composition
 			continue
 		songSoup = BeautifulSoup(songPage.content, 'html.parser')
-		# print songSoup.prettify()
 		stanzaFlag = songSoup.find(string=re.compile(partOfSong))
-		# print stanzaFlag
 		if(stanzaFlag == None):
 			print "Error: could not find stanza flag: " + partOfSong
 		else:
-			# type(stanzaFlag)
 			line = stanzaFlag.next_sibling.contents[0].strip()
-			# print type(line)
 			lyrics[composer].update(re.findall(r'\w+', line))
-		# line = songSoup.find(string=re.compile(pallavi)).next_sibling.contents[0].strip()
-		# lyrics[composer].update(re.find_all(r'\w+', line.lower()))
 
-
-print "Thyagaraja: " 
-print lyrics["Thyagaraja"].most_common(10)
-print "ShyamaShaastri: " 
-print lyrics["ShyamaShaastri"].most_common(10)
-print "Ramadasu: "
-print lyrics["Ramadasu"].most_common(10)
-print "Annamayya: "
-print lyrics["Annamayya"].most_common(10)
+for composer in lyrics.iterkeys():
+	print composer
+	print lyrics[composer].most_common(10)
